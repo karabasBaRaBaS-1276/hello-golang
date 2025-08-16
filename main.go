7b97b9b1504905174abd65911806d3731dfe6840
Осенчаков Роман <rongo@yandex.ru>
2025-08-16 13:59:41 +0300

    hello go

--- main.go
+++ main.go
@@ -1,28 +1,11 @@
 package main
 
-import (
-    "fmt"
-    "sync"
-)
+import "fmt"
 
 func main() {
-    var wg sync.WaitGroup
-    wg.Add(5)
-
-    for i := 0; i < 5; i++ {
-        go func() {
-            fmt.Println(i)
-            wg.Done()
-        }()
-    }
-
-    wg.Wait()
+    fmt.Println(hello())
 }
 
-func MaxInt(a, b int) int {
-    if a >= b {
-        return a
-    }
-
-    return b
+func hello() string {
+    return "Hello go"
 }
